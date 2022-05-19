1380f8ddae6e3ad8fc399967d4d7caf11a0d0eae
20D070026 <81476319+20D070026@users.noreply.github.com>
2022-05-19 20:43:56 +0530

    Delete hyperloop directory

--- hyperloop/contactForm/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.contrib import admin
-from . import views
-
-urlpatterns = [
-    path('', views.form_view),
-    path('submitted', views.form_data_view),
-]
